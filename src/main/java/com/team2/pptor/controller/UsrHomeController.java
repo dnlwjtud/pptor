fbcddda116d7440620fbf2de157e3ac01c8513a0
dnlwjtud <archaco124@gmail.com>
2021-08-10 16:40:43 +0900

    Controller setting

--- src/main/java/com/team2/pptor/controller/UsrHomeController.java
+++ src/main/java/com/team2/pptor/controller/UsrHomeController.java
@@ -1,4 +1,13 @@
 package com.team2.pptor.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UsrHomeController {
+
+
+    public UsrHomeController() {
+    }
+
+
 }
