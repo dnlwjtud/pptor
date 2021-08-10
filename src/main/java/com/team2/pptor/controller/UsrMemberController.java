2f5b4361cb70e48b73726239a82be97bed2a46ec
inghyee <inghyee@gmail.com>
2021-08-10 18:40:19 +0900

    Controller Mapping

--- src/main/java/com/team2/pptor/controller/UsrMemberController.java
+++ src/main/java/com/team2/pptor/controller/UsrMemberController.java
@@ -1,7 +1,23 @@
 package com.team2.pptor.controller;
 
+import com.team2.pptor.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsrMemberController {
+    private MemberService memberService;
+
+    @Autowired
+    public UsrMemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    //로그인
+    @GetMapping("usr/member/login")
+    public String loginPage() {
+        return "login";
+    }
+
 }
