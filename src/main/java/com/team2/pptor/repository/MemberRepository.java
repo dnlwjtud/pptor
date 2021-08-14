0c72a19464c01ae2627c23d820fb4b8ab7635d52
bgchoi205 <choibk4938@gmail.com>
2021-08-14 18:06:15 +0900

    UPDATE : MemberRepository에 join, delete, findById 추가

--- src/main/java/com/team2/pptor/repository/MemberRepository.java
+++ src/main/java/com/team2/pptor/repository/MemberRepository.java
@@ -1,8 +1,33 @@
 package com.team2.pptor.repository;
 
+import com.team2.pptor.domain.Member;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Repository
 public class MemberRepository {
 
+    @PersistenceContext
+    EntityManager em;
+
+    public int join(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public int delete(Member member){
+        em.remove(member);
+        return member.getId();
+    }
+
+    // update 고민중
+//    public int update(Member member){
+//
+//    }
+
+    public Member findById(int id){
+        return em.find(Member.class, id);
+    }
 }
