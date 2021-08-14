d1930abf397cf214198e5c094f17c0f228fac62b
dnlwjtud <archaco124@gmail.com>
2021-08-14 16:31:32 +0900

    UPDATE : application.yml 설정 수정

--- ddl/db.sql
+++ ddl/db.sql
@@ -7,15 +7,3 @@ ON *.*
 TO `team`@`%`
 IDENTIFIED BY 'two';
 
-#CREATE TABLE `member` (
-#    `id` INT(100) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-#    `loginId` VARCHAR(100) NOT NULL,
-#    `loginPw` VARCHAR(100) NOT NULL,
-#    `name` VARCHAR(100) NOT NULL,
-#    `nickname` VARCHAR(100) NOT NULL,
-#    `email` VARCHAR(100) NOT NULL,
-#    `regDate` DATETIME NOT NULL,
-#    `updateDate` DATETIME NOT NULL,
-#    `blind` TINYINT(2) NOT NULL DEFAULT 0,
-#    `authLevel` SMALLINT(2) UNSIGNED NOT NULL DEFAULT 3
-#);
