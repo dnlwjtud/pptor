a2c2e06627a9b7ee2bd3f68db96d1eb5bda10079
bgchoi205 <choibk4938@gmail.com>
2021-08-17 16:29:00 +0900

    UPDATE : MemberRepository에 메서드 추가, 수정

--- src/main/java/com/team2/pptor/repository/MemberRepository.java
+++ src/main/java/com/team2/pptor/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -12,22 +14,34 @@ public class MemberRepository {
     @PersistenceContext
     EntityManager em;
 
+    // 회원가입
+    @Transactional
     public int join(Member member){
         em.persist(member);
         return member.getId();
     }
 
+    // 회원탈퇴
+    @Transactional
     public int delete(Member member){
         em.remove(member);
         return member.getId();
     }
 
-    // update 고민중
-//    public int update(Member member){
+//    public int modify(Member member){
 //
 //    }
 
+    // 회원번호로 회원찾기
+    @Transactional
     public Member findById(int id){
         return em.find(Member.class, id);
+    }
+
+    // 회원 전체 리스트
+    @Transactional
+    public List findAll(){
+        return em.createQuery("SELECT m FROM Member m")
+                .getResultList();
     }
 }
