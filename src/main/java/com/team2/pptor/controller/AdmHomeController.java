1bed9d477d555352293796ec8cd327c283928673
inghyee <inghyee@gmail.com>
2021-09-05 03:11:28 +0900

    UPDATE : 사이드바 레이아웃 수정, 고정 시 발생하는 이슈 해결 중

--- src/main/java/com/team2/pptor/controller/AdmHomeController.java
+++ src/main/java/com/team2/pptor/controller/AdmHomeController.java
@@ -1,14 +1,9 @@
 package com.team2.pptor.controller;
 
-import com.team2.pptor.domain.Member.Member;
-import com.team2.pptor.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,7 +14,7 @@ public class AdmHomeController {
     관리자 홈
     */
     @GetMapping("/adm")
-    public String showMemberManage(){
+    public String showHome(){
 
         return "adm/home";
     }
