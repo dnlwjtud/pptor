92c518b59cd5a9e769014a798ea30835762cc3ce
dnlwjtud <archaco124@gmail.com>
2021-09-07 00:31:11 +0900

    UPDATE : ArticleRepository를 Spring Data JPA로 변경 완료

--- src/main/java/com/team2/pptor/repository/ArticleRepository.java
+++ src/main/java/com/team2/pptor/repository/ArticleRepository.java
@@ -5,9 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends JpaRepository < Article, Long > {
 
-    List<Article> findAll();
+    /*
+    @Query("select a from Article a where a.title = :title")
+    List<Article> findArticlesByTitle ( @Param("title") String title );
+     */
 
+    // LIKE % 검색어 % 
+    List<Article> findByTitleContaining(String title);
+
+    // title 단건 조회
+    Article findArticleByTitle (String title);
+
+    // id 단건 조히
+    Article findArticleById(Long id);
 
 }
--- src/main/java/com/team2/pptor/service/ArticleService.java
+++ src/main/java/com/team2/pptor/service/ArticleService.java
@@ -4,29 +4,39 @@ import com.team2.pptor.domain.Article.Article;
 import com.team2.pptor.domain.Article.ArticleModifyForm;
 import com.team2.pptor.domain.Member.Member;
 import com.team2.pptor.repository.ArticleJpaRepository;
+import com.team2.pptor.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ArticleService {
 
-    public final ArticleJpaRepository articleRepository;
+    // public final ArticleJpaRepository articleRepository;
+    private final ArticleRepository articleRepository;
 
     /*
     게시물 조회
      */
     public Article findById(Long id) {
 
+        Optional<Article> findArticle = articleRepository.findById(id);
+
+        return findArticle.orElseThrow(() -> new NoSuchElementException("해당 게시물은 존재하지 않습니다"));
+
+        /*
         try {
-            return articleRepository.findById(id);
+            return articleRepository.findArticleById(id);
         } catch (Exception e ) {
             throw new IllegalStateException("존재하지 않는 게시물입니다.");
         }
+        */
 
     }
 
@@ -34,33 +44,55 @@ public class ArticleService {
     게시물 작성
      */
     @Transactional
-    public void save(Article article) {
-        articleRepository.save(article);
+    public Article save(Article article) {
+        return articleRepository.save(article);
     }
 
     /*
     게시물 수정
-     */
+    */
     @Transactional
-    public Long modify(ArticleModifyForm articleModifyForm, Member member){
+    public void modify(ArticleModifyForm articleModifyForm, Member member){
+
+        Article findArticle = articleRepository.findArticleById(articleModifyForm.getId());
+
+        if ( findArticle != null ) {
+            findArticle.modifyArticle(
+                    articleModifyForm.getTitle(),
+                    articleModifyForm.getMarkdown(),
+                    articleModifyForm.getHtml(),
+                    member
+            );
+        } else {
+            throw new NoSuchElementException("해당 게시물은 존재하지 않습니다.");
+        }
+
+
+        /*
+        상황에 맞는 Optional 사용이 어려움
 
         // 게시물 번호로 게시물의 정보를 꺼냄
-        Article article = articleRepository.findById(articleModifyForm.getId());
+        //Article article = articleRepository.findById(articleModifyForm.getId());
 
-        article.modifyArticle(
-                articleModifyForm.getTitle(),
-                articleModifyForm.getMarkdown(),
-                articleModifyForm.getHtml(),
-                member
+        Optional<Article> articleOptional = articleRepository.findById(articleModifyForm.getId());
+
+        articleOptional.ifPresent(
+                article -> {
+                    article.modifyArticle(
+                            articleModifyForm.getTitle(),
+                            articleModifyForm.getMarkdown(),
+                            articleModifyForm.getHtml(),
+                            member
+                    );
+                }
+                
         );
-
-        return article.getId();
+         */
 
     }
 
-
     /*
-    게시물 삭제
+    게시물 번호로 삭제
      */
     @Transactional
     public void delete(Long id){
@@ -81,4 +113,12 @@ public class ArticleService {
     public List<Article> list() {
         return articleRepository.findAll();
     }
+
+    /*
+    제목으로 게시물 검색
+     */
+    List<Article> findByTitleContaining(String title) {
+        return articleRepository.findByTitleContaining(title);
+    }
+
 }
